f7898266a690a9ba5bb276e09027c8a949d6ccd6
lcmomo <1494145756@qq.com>
2024-04-10 10:23:00 +0800

    feat: 调整目录结构

--- projects/momo-color-picker/src/color-picker/color-picker.module.ts
+++ projects/momo-color-picker/src/color-picker/color-picker.module.ts
@@ -6,9 +6,9 @@ import { ZORRO_MODULES } from './nz-antd.module';
 import { BaseGradientComponent, BaseHandlerComponent, BasePaletteComponent, BasePickerComponent, BaseSliderComponent } from './components';
 import { NzColorPickerComponent } from './color-picker.component';
 import { NgAntdColorPickerComponent } from "./color-picker/ng-antd-color-picker.component";
-import { NzColorFormatComponent } from '../color-picker/color-format/color-format.component';
-import { NzColorBlockComponent } from '../color-picker/color-block/color-block.component';
-import { NgAntdColorBlockComponent } from '../color-picker/color-block/ng-antd-color-block/ng-antd-color-block.component';
+import { NzColorFormatComponent } from './color-format/color-format.component';
+import { NzColorBlockComponent } from './color-block/color-block.component';
+import { NgAntdColorBlockComponent } from './color-block/ng-antd-color-block/ng-antd-color-block.component';
 const COMPONENTS = [
   BaseGradientComponent,
   BaseHandlerComponent,
